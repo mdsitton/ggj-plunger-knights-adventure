7124857d64ef54dd3f000ccfeb0aae9b37777628
LockeKhushrenada <24321637+LockeKhushrenada@users.noreply.github.com>
2024-01-27 13:27:51 -0600

    Key Prefabs

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : BaseItem, IItem
+{
+    public string Name { get; set; }
+
+    public ItemTypes ItemType => ItemTypes.Key;
+
+    protected override void OnPrimary()
+    {
+        
+    }
+
+    protected override void OnQuaternary()
+    {
+        
+    }
+
+    protected override void OnSecondary()
+    {
+        
+    }
+
+    protected override void OnTertiary()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
