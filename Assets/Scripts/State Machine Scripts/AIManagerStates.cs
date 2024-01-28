cc75814cf9ffcb2cd2c6a78d05d48e0b50599038
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2024-01-27 23:33:44 -0600

    Baground

--- Assets/Scripts/State Machine Scripts/AIManagerStates.cs
+++ Assets/Scripts/State Machine Scripts/AIManagerStates.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class AIManagerStates : MonoBehaviour
 {
-    //public states[]
+    public States[] actionList;
+
+    void Awake()
+    {
+        Debug.Log("This is a action test" + actionList[0]);
+    }
+
 }
