f3aedc22986d378a2de6cfcdacf4c29b3d47125c
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2024-01-27 23:10:55 -0600

    Background And Collider
    Fix Background.
    
    Bug projectile Collider

--- Assets/Scripts/State Machine Scripts/AttackState.cs
+++ Assets/Scripts/State Machine Scripts/AttackState.cs
@@ -1,12 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackState : States
 {
+    public int CurrentTarget;
+    public float attackRadius = 0.5f;
     public override States RunCurrentState()
     {
-        Debug.Log("I have attacked");
+        if (true)
+        {
+            if (!FindPlayerInRadius(attackRadius))
+            {
+                Debug.Log("I have attacked");
+            }
+        }
         return this;
     }
+    private bool FindPlayerInRadius(float radius)
+    {
+        OnDrawGizmosSelected();
+
+        var colliders = Physics2D.OverlapCircleAll(this.gameObject.transform.position, radius);
+        foreach (var collider in colliders)
+        {
+            var attackable = collider.gameObject.GetComponent<IAttackable>();
+            if (attackable != null && attackable.EntityType == EntityType.Player)
+            {
+                //CurrentTarget = collider.gameObject.GetComponent<IAttackable>();
+                return true;
+            }
+        }
+        return false;
+    }
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+    }
+
+
 }
