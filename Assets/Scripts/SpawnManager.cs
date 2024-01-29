efbb23a04c1802936e7fad64ea6a54ae9eabc42c
Olmer "Sylastial <olmercr@gmail.com>
2024-01-29 15:46:52 -0600

    Updated SpawnManager.CS Win Condition
    Player wins now when all monster track in SpawnManager.CS in GAME LOGIC has been killed.
    
    Fix bug when list being track is Empty.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,29 +9,48 @@ public class SpawnManager : MonoBehaviour
 
     private void Awake()
     {
+        //Create a bool to track all enemies in List then we set if alive or not
         enemiesAlive = new bool[keepTrackOfEnemiesList.Length];
-        foreach (GameObject inGameEnimies in keepTrackOfEnemiesList)
+        for (int i = 0; i < keepTrackOfEnemiesList.Length; i++)
         {
-            enemiesAlive = new bool[keepTrackOfEnemiesList.Length];
+            enemiesAlive[i] = keepTrackOfEnemiesList[i].activeSelf;
         }
     }
     
-
-    private void LateUpdate()
+    private void LateUpdate()//No need check every Update
     {
-        if (!keepTrackOfEnemiesList[0].gameObject.activeSelf)
+        Debug.Log("Are All Enimes being Track Dead = " + NoEnemiesAlive().ToString());
+        //if Enimes all Die and Keeping Track List is not Empty
+        if (NoEnemiesAlive() == true && keepTrackOfEnemiesList[0] != null)
         {
-            enemiesAlive[0] = false;
-        }
-        else
-        {
-            enemiesAlive[0] = true;
-        }
-
-        if (!enemiesAlive[0])
-        {
-            Debug.Log("Game Over");
+            Debug.Log("Are All Enimes being Track Finally Died = " + NoEnemiesAlive().ToString());
+            Debug.Log("Game Over or Player Wins");
             SceneManager.LoadScene(2);
         }
+    }
+
+    private bool NoEnemiesAlive()
+    {
+        bool allEnemiesDied = true;
+        //Check the list of Enemies to see if still in game if not set in Array
+        for (int i = 0; i < keepTrackOfEnemiesList.Length; i++)
+        {
+            enemiesAlive[i] = keepTrackOfEnemiesList[i].activeSelf;
+        }
+
+        //if any enemy is still alive we set set bool to false
+        foreach (var enemy in enemiesAlive)
+        {
+            if (enemy != false)
+            {
+                allEnemiesDied = false;
+                break;
+            }
+        }
+        return allEnemiesDied;
+
+        //This returs game wincodintion if any of Monster are dead
+       // bool CurrentCondition = enemiesAlive.All(x => x);
+       // return CurrentCondition;
     }
 }
